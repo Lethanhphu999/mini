9d81faf323a44a1cb927172ea4b615eb4cd31c7f
LeThanhPho <97968973+Lethanhphu999@users.noreply.github.com>
2024-04-09 17:37:19 +0700

    inital commit

---
+++ mini/main.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    QWidget window;
+
+    QPushButton* button = new QPushButton("Hello World", &window);
+    button->setGeometry(10, 10, 80, 30);
+
+    window.show();
+    return app.exec();
+}
